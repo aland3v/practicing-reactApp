b7f0db3860743f0c01e29fbe2b26036689cc1192
Alan dev <aland3v@gmail.com>
2020-12-30 19:07:39 -0400

    Creación de componentes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react'
-/* function HelloWorld(props) {
-  console.log(props);
-  return (
-    <div id="hello">
-    <h3>{props.subtitle}</h3>
-    {props.myText}
-    </div>
-  )
-} */
-
-class HelloWorld extends React.Component {
+import React, { Component } from 'react'
+import tasks from './sample/tasks.json'
+import Tasks from './components/tasks'
+// console.log(tasks);
+class App extends Component {
 
   state = {
-    show: true
-  }
-
-  /* toggleShow(){
-    this.setState({show:false})
-  } */
-
-  toggleShow = () => {
-    this.setState({show:!this.state.show})
+    tasks: tasks
   }
 
   render() {
-    if (this.state.show) {
-      return (
-        <div id="hello">
-          <h3>{this.props.subtitle}</h3>
-          {this.props.myText}
-          <button onClick={this.toggleShow}>Toggle show</button>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <h1>There are not Elements</h1>
-          <button onClick={this.toggleShow}>Toggle show</button>
-        </div>
-      )
-    }
-  }
-}
-
-function App() {
-  return (
-    <div>
-    This is my component:
-    <HelloWorld myText='hola Alan' subtitle='algo' />
-    <HelloWorld myText='hola Pepe' subtitle='nuevo' />
-    <HelloWorld myText='hola Ana' subtitle='sucederá' />
+    return <div>
+      <Tasks tasks={this.state.tasks}/>
     </div>
-  );
+  }
 }
 
 export default App;
